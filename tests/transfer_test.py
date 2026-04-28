6ed198cd0045120f2d77330d74dd365cc981c59f
VladislavDevs <supermars5327@gmail.com>
2026-04-28 22:43:35 +0700

    Updated transfer_test.py

--- tests/transfer_test.py
+++ tests/transfer_test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
